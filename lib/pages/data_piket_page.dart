8446b1e954f3ccac4517d26a6bd7c8f945d8c3b5
RezaAzharii <rezaazhari867@gmail.com>
2025-04-28 19:16:25 +0700

    Membuat halaman DataPiketPage dan menangkap parameter dari halaman Home

---
+++ lib/pages/data_piket_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DataPiketPage extends StatefulWidget {
+  final String namaAkun;
+  const DataPiketPage({
+    super.key,
+    required this.namaAkun
+  });
+
+  @override
+  State<DataPiketPage> createState() => _DataPiketPageState();
+}
+
+class _DataPiketPageState extends State<DataPiketPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
