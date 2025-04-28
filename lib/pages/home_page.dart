52e74c3e4395c04eda2109d16811ce2052c86e5f
RezaAzharii <rezaazhari867@gmail.com>
2025-04-28 17:45:42 +0700

    Membuat HomePage dengan parameter akun dan layout Scaffold

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final String akun;
+  const HomePage({super.key, required this.akun});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
