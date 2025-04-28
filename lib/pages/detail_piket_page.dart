c290179c9e2e1f3d5f31e044933b18b9d8d59ac2
RezaAzharii <rezaazhari867@gmail.com>
2025-04-28 20:52:54 +0700

    Menambahkan parameter untuk menangkap parameter yang dikirim dari datapiketpage

---
+++ lib/pages/detail_piket_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  final String namaAnggota;
+  final String tanggalPiket;
+  final String tugasPiket;
+  const DetailPiketPage({
+    super.key,
+    required this.namaAnggota,
+    required this.tanggalPiket,
+    required this.tugasPiket,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
