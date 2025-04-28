d67ad7befe56f615fbc74c277e15f41f50581510
RezaAzharii <rezaazhari867@gmail.com>
2025-04-28 17:30:32 +0700

    Menambahkan controller, form key dan toggle obscurePassword

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _namaLengkapController = TextEditingController();
+  final _noHpController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _passwordConfirmController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _obscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
