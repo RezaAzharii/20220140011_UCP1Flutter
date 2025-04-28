f1dd7f051c6ecb3a7203b745caf5965a56f139db
RezaAzharii <rezaazhari867@gmail.com>
2025-04-28 17:49:13 +0700

    Menambahkan header selamat datang, avatar profil, nama akun, dan tombol logout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,53 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 30, bottom: 18, left: 10, right: 10),
+            width: double.infinity,
+            color: Colors.deepOrange,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 15,
+              children: [
+                Row(
+                  spacing: 15,
+                  children: [
+                    CircleAvatar(
+                      radius: 35,
+                      backgroundImage: AssetImage('assets/images/bob3.jpg'),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Selamat Datang',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          widget.akun,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.logout, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
