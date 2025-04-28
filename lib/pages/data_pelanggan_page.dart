2dbafb1132669adf8cf73b115105bf60577fd312
RezaAzharii <rezaazhari867@gmail.com>
2025-04-28 22:18:26 +0700

    Menambahkan Validasi untuk form input

--- lib/pages/data_pelanggan_page.dart
+++ lib/pages/data_pelanggan_page.dart
@@ -16,6 +16,55 @@ class _DataPelangganPageState extends State<DataPelangganPage> {
   final _kodePosController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
+  String? _validasiNamaCustomer (String? value){
+    if(value == null || value.isEmpty){
+      return 'Nama tidak boleh kosong';
+    }
+    return null;
+  }
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    }
+    if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? _validateNoHp(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'No HP wajib diisi';
+    }
+    if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+      return 'No HP harus berupa angka';
+    }
+    if (value.length < 10 || value.length > 13) {
+      return 'No HP harus 10-13 digit';
+    }
+    return null;
+  }
+
+  String? _validasiAlamat (String? value){
+    if(value == null || value.isEmpty){
+      return 'Alamat tidak boleh kosong';
+    }
+    return null;
+  }
+  String? _validasiProvinsi (String? value){
+    if(value == null || value.isEmpty){
+      return 'Provinsi tidak boleh kosong';
+    }
+    return null;
+  }
+  String? _validasiKodePos (String? value){
+    if(value == null || value.isEmpty){
+      return 'Kode Pos tidak boleh kosong';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
