635195050868c894d061b2100718335c5ef2f99b
RezaAzharii <rezaazhari867@gmail.com>
2025-04-28 22:49:48 +0700

    Menambahkan parameter untuk menangkap parameter yang dikirim dari data Pelanggan

---
+++ lib/pages/profile_pelanggan_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ProfilePelangganPage extends StatelessWidget {
+  final String namaCustomer;
+  final String email;
+  final String noHp;
+  final String alamat;
+  final String provinsi;
+  final String kodePos;
+  const ProfilePelangganPage({
+    super.key,
+    required this.namaCustomer,
+    required this.email,
+    required this.noHp,
+    required this.alamat,
+    required this.provinsi,
+    required this.kodePos
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
