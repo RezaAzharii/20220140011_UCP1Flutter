98c3971c2728584e29b7b754217dcecdb910519a
RezaAzharii <rezaazhari867@gmail.com>
2025-04-29 11:59:59 +0700

    Menambahkan Form beserta input seperti datepicker, dropdown, dan button submit

--- lib/pages/data_barang_page.dart
+++ lib/pages/data_barang_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DataBarangPage extends StatefulWidget {
   const DataBarangPage({super.key});
@@ -81,6 +82,183 @@ class _DataBarangPageState extends State<DataBarangPage> {
         title: const Text(
           'Pendataan Barang',
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'Tanggal Transaksi',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: _tanggalTransaksiController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.event_available_outlined),
+                  hintText: "Tanggal Transaksi",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                readOnly: true,
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null) {
+                    setState(() {
+                      _tanggalTransaksiController.text = DateFormat(
+                        "EEEE, dd MMMM yyyy",
+                        "id_ID",
+                      ).format(pickedDate);
+                    });
+                  }
+                },
+                validator: _validateTanggal,
+              ),
+              const SizedBox(height: 30),
+              DropdownButtonFormField<String>(
+                value: _jenisTransaksiController,
+                hint: const Text('Pilih jenis transaksi'),
+                items:
+                    jenisTransaksi.map((value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    _jenisTransaksiController = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Jenis Transaksi',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                validator: _validateJenistransaksi,
+              ),
+              const SizedBox(height: 30),
+              DropdownButtonFormField<String>(
+                value: _jenisBarangController,
+                hint: Text('Pilih jenis barang'),
+                items:
+                    jenisBarang.map((value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    _jenisBarangController = value;
+                    if (value != null) {
+                      _hargaSatuanController.text =
+                          hargaBarang[value]!.toString();
+                    }
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Jenis Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                validator: _validateJensibarang,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Jumlah Barang',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 8),
+                        TextFormField(
+                          controller: _jumlahBarangController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintText: 'Jumlah barang',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                          ),
+                          validator: _validateJumlahBarang,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Harga Satuan',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 8),
+                        TextFormField(
+                          controller: _hargaSatuanController,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            prefixText: 'Rp. ',
+                            hintText: 'Harga Satuan',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                          ),
+                          validator: _validateHargaSatuan,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
