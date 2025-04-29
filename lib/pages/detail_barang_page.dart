ac87efbb055d787d928a992126e0d3aadc1204c3
RezaAzharii <rezaazhari867@gmail.com>
2025-04-29 12:04:31 +0700

    Menambahkan icon di center dan heading text yang dibungkus widget Column

--- lib/pages/detail_barang_page.dart
+++ lib/pages/detail_barang_page.dart
@@ -20,7 +20,44 @@ class DetailBarangPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start, 
+          children: [
+            const SizedBox(height: 40),
+            Container(
+              padding: const EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.green[100]!,
+                  width: 3,
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green.withOpacity(0.1),
+                ),
+                child: const Icon(
+                  Icons.check_circle_rounded,
+                  color: Colors.green,
+                  size: 130,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Data Berhasil Disimpan',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
